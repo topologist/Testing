804eb13c7b766fa2794536290860c28c943b3805
Ryan Connelly <a.topol0gist@gmail.com>
2011-12-13 00:04:26 +1100

    Testing syntax highlighting and other things.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "Vec3f.h"
 
-int main()
+int main(void)
 {
-	std::cout << "Hello, world!\n";
+	Vec3f v1(1.0f, 3.14f, 2.17f);
+	Vec3f v2 = v1.Normalize();
+
+	std::cout << "v1 [" << v1.x << " " << v1.y << " " << v1.z << "]\n";
+	std::cout << "v2 [" << v2.x << " " << v2.y << " " << v2.z << "]\n";
+
 	return 0;
 }
